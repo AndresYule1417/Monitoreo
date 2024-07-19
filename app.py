d4dcf158a69206224a05ef511b2eff24d1bbe223
Andrés  Yule <andres.yule@campusucc.edu.co>
2024-07-18 19:52:47 -0500

    Creación del correo de la plataforma para la sección de "olvidaste contraseña"

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ app.secret_key = "tu_clave_secreta"
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'afelipey1417@gmail.com'
-app.config['MAIL_PASSWORD'] = 'rjwq okzb unip aqdz'
-app.config['MAIL_DEFAULT_SENDER'] = 'afelipey1417@gmail.com'
+app.config['MAIL_USERNAME'] = 'hidricosrecursos1@gmail.com'
+app.config['MAIL_PASSWORD'] = 'vsnw lbwh xscv tcea'
+app.config['MAIL_DEFAULT_SENDER'] = 'hidricosrecursos1@gmail.com'
 mail = Mail(app)
 
 def get_db_connection():
