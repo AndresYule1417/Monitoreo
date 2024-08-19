965914967ccfa77ac1dcfe85b4f2172c3e4cebb6
mypez1987 <cristiandaniel01@hotmail.com>
2024-08-19 09:44:12 -0500

    documentacion
    documentacion

--- Monitoreo_V2/api_monitoreo_hidrico/manage.py
+++ Monitoreo_V2/api_monitoreo_hidrico/manage.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+#Funcion principal para poder manejar desde la consola los comandos de:
+#python manage.py migrate
+#python manage.py makemigrations
+#python manage.py runserver
 
-def main():
-    """Run administrative tasks."""
+def main():    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_monitoreo_hidrico.settings')
     try:
         from django.core.management import execute_from_command_line
