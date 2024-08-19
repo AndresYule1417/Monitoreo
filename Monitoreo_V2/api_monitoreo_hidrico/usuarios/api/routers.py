eced30ac6988ba6872702f9d91887ddf3cdf61b9
mypez1987 <cristiandaniel01@hotmail.com>
2024-08-19 10:19:01 -0500

    documentacion
    documentacion

--- Monitoreo_V2/api_monitoreo_hidrico/usuarios/api/routers.py
+++ Monitoreo_V2/api_monitoreo_hidrico/usuarios/api/routers.py
@@ -1,3 +1,5 @@
+#archivo q contiene la ruta para el llamdo del cliente el cual gestiona las peticiones 
+#GET, POST, PUT, DELETE
 from rest_framework.routers import DefaultRouter
 from usuarios.api.viewsets import UserViewSet
 
