e3bf38e78df288745d180ac1d37bf98ce778b76f
Juan Pablo <40966420+juanpac96@users.noreply.github.com>
2025-03-19 09:15:46 -0500

    Eliminando carpeta manualmente

--- client_rendimiento_hidrico/src/app/shared/models/user-login.ts
+++
@@ -1,6 +0,0 @@
-export interface UserLogin {
-    token: string;
-    refresh_token: string;
-    user: any[];
-    message: string;
-}
--- client_rendimiento_hidrico/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    //API_URL: 'http://10.10.10.3:8000',
-    API_URL: 'http://localhost:8000',
-};
