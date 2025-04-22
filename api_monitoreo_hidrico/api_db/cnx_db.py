83ec7d2ec3860c44123e2d38c6f849d175bdce4e
mypez1987 <cristiandaniel01@hotmail.com>
2025-04-22 18:16:27 -0500

    cambios
    cambios

--- api_monitoreo_hidrico/api_db/cnx_db.py
+++ api_monitoreo_hidrico/api_db/cnx_db.py
@@ -9,7 +9,7 @@ class CnxDb():
             self.cnx = psycopg2.connect(database='monitoreo_hidrico_v3', user='postgres', host='127.0.0.1', password='12345', port='5432')               
         except (psycopg2.DatabaseError, Exception) as error:
             self.cnx = None
-            print(error)     #Ingeniero cabeza de leon
+            print(error)     
     
     def get_cnx(self):
         if(self.cnx):
@@ -20,4 +20,28 @@ class CnxDb():
         cursor = self.cnx.cursor()
         self.cnx.autocommit = False
         cursor.execute(sql)
-        self.cnx.commit()      
+        self.cnx.commit()   
+
+class CnxDb_SZH():
+    cnx = None    
+    
+    def __init__(self) -> None:
+        try:
+            self.cnx = psycopg2.connect(database='db_szh_coello_ohts', user='postgres', host='127.0.0.1', password='12345', port='5432')               
+        except (psycopg2.DatabaseError, Exception) as error:
+            self.cnx = None
+            print(error)  
+
+    def get_cnx(self):
+        if(self.cnx):
+            return self.cnx
+        return None
+    
+    def getQueryAll(self, sql):
+        cursor = self.cnx.cursor(cursor_factory=RealDictCursor)
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        if(self.cnx):
+            cursor.close()
+        return rows
+      
--- api_monitoreo_hidrico/indice_uso/api/viewsets.py
+++ api_monitoreo_hidrico/indice_uso/api/viewsets.py
@@ -2,7 +2,38 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
 
+import pandas as pd
+from natsort import natsorted
+
+from api_db.cnx_db import CnxDb_SZH
+
 class IndiceUsoViewSet(viewsets.GenericViewSet):
     def list(self, request):
-        print("entre")
-        return Response({'message': 'Proyecto Reguistrado'}, status=status.HTTP_200_OK)
+        sql = "select nombre_sub_zona, metrica, valor \
+            from metricas_hidrologicas \
+            left join sub_zonas_hidrograficas \
+            on id_sub_cuenca = id_sub_zona \
+            where metrica in ('CA', 'DH_M3PS','OHRD_N','IUA_N','CAT_IUA_N','Cat-IUA_N')"
+        cnxDb = CnxDb_SZH()
+        print(cnxDb.get_cnx)
+        if(cnxDb.get_cnx):
+            result = cnxDb.getQueryAll(sql)
+            data = pd.DataFrame(result)   
+
+            data_pivot = data.pivot(index="nombre_sub_zona", columns="metrica", values="valor")
+            data_pivot.columns.name = None
+            orden = natsorted(data_pivot.index, reverse=True)           
+            data_pivot.index = pd.CategoricalIndex(data_pivot.index, categories=orden, ordered=True)          
+            data_pivot= data_pivot.sort_index()    
+            print(data_pivot)        
+
+            for col in data_pivot.columns:
+                try:
+                    data_pivot[col] = pd.to_numeric(data_pivot[col])
+                except ValueError:
+                    pass  # Si la conversión falla, significa que la columna no es numérica            
+            
+            data_pivot = data_pivot.round(3)             
+            print(data_pivot)
+
+            return Response({'message':'success', 'data':data_pivot}, status=status.HTTP_200_OK)
