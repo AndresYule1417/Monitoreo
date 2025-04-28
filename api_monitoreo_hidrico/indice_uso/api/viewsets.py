a7c2b74ac30fc201a186e40a58471c8cd82c2507
mypez1987 <cristiandaniel01@hotmail.com>
2025-04-28 13:56:39 -0500

    cambios
    cambios

--- api_monitoreo_hidrico/indice_uso/api/viewsets.py
+++ api_monitoreo_hidrico/indice_uso/api/viewsets.py
@@ -14,8 +14,7 @@ class IndiceUsoViewSet(viewsets.GenericViewSet):
             left join sub_zonas_hidrograficas \
             on id_sub_cuenca = id_sub_zona \
             where metrica in ('CA', 'DH_M3PS','OHRD_N','IUA_N','CAT_IUA_N','Cat-IUA_N')"
-        cnxDb = CnxDb_SZH()
-        print(cnxDb.get_cnx)
+        cnxDb = CnxDb_SZH()        
         if(cnxDb.get_cnx):
             result = cnxDb.getQueryAll(sql)
             data = pd.DataFrame(result)   
@@ -24,8 +23,7 @@ class IndiceUsoViewSet(viewsets.GenericViewSet):
             data_pivot.columns.name = None
             orden = natsorted(data_pivot.index, reverse=True)           
             data_pivot.index = pd.CategoricalIndex(data_pivot.index, categories=orden, ordered=True)          
-            data_pivot= data_pivot.sort_index()    
-            print(data_pivot)        
+            data_pivot= data_pivot.sort_index()                    
 
             for col in data_pivot.columns:
                 try:
@@ -33,7 +31,38 @@ class IndiceUsoViewSet(viewsets.GenericViewSet):
                 except ValueError:
                     pass  # Si la conversión falla, significa que la columna no es numérica            
             
-            data_pivot = data_pivot.round(3)             
-            print(data_pivot)
+            data_pivot = data_pivot.round(3)   
+            data_pivot_an = data_pivot.reset_index()  
+            
 
-            return Response({'message':'success', 'data':data_pivot}, status=status.HTTP_200_OK)
+        sql = "SELECT nombre_sub_zona, metrica, valor \
+            FROM metricas_hidrologicas \
+            LEFT JOIN sub_zonas_hidrograficas \
+            ON id_sub_cuenca = id_sub_zona \
+            WHERE metrica IN ('DH_M3PS', 'CA_S', 'OHRD_S', 'IUA_S', 'CAT_IUA_S', 'Cat-IUA_S')"    
+
+         
+        
+        if(cnxDb.get_cnx):
+            result = cnxDb.getQueryAll(sql)
+            data = pd.DataFrame(result)
+
+            data_pivot = data.pivot(index="nombre_sub_zona", columns="metrica", values="valor")
+            data_pivot.columns.name = None
+            orden = natsorted(data_pivot.index, reverse=True)           
+            data_pivot.index = pd.CategoricalIndex(data_pivot.index, categories=orden, ordered=True)          
+            data_pivot= data_pivot.sort_index()                    
+
+            for col in data_pivot.columns:
+                try:
+                    data_pivot[col] = pd.to_numeric(data_pivot[col])
+                except ValueError:
+                    pass  # Si la conversión falla, significa que la columna no es numérica            
+            
+            data_pivot = data_pivot.round(3)   
+            data_pivot_as = data_pivot.reset_index()
+        
+        return Response({'message':'success', 'data_pivot_an':data_pivot_an, 'data_pivot_as':data_pivot_as}, status=status.HTTP_200_OK) 
+                
+
+            
